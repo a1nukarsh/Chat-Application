fb3a48ebece4204c18bddf54f12744cd5449460f
Anukarsh <anukarshr@gmail.com>
2021-09-19 10:22:58 +0530

    update

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -3,7 +3,14 @@ const io = require("socket.io")(8000);
 
 const users = {};
 io.on("connection", (socket) => {
-  socket.on("user-joined", (name) => {
-    user[socket.id] = name;
+  socket.on("new-user-joined", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined");
+  });
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: user[socket.id],
+    });
   });
 });
