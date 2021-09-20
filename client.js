73c9091247a241c47d0fef12312d6194e278e0c5
Anukarsh <anukarshr@gmail.com>
2021-09-20 19:32:35 +0530

    update

---
+++ client.js
@@ -0,0 +1,7 @@
+const socket = io('http://localhost:8000')
+
+const form = document.getElementById('send-container')
+const msgInp = document.getElementById('msgInp')
+const msgContainer = document.getElementById('m-container')
+
+
