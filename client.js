5a3336e2c79a70e06ccdb3a6e5dcb455367d0378
Anukarsh <anukarshr@gmail.com>
2021-09-20 23:30:38 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,9 +1,8 @@
-const socket = io('http://localhost:8000')
+const socket = io("http://localhost:8000");
 
-const form = document.getElementById('send-container')
-const msgInp = document.getElementById('msgInp')
-const msgContainer = document.getElementById('m-container')
+const form = document.getElementById("send-container");
+const msgInp = document.getElementById("msgInp");
+const msgContainer = document.getElementById("m-container");
 
-const name = prompt("Enter your name to Join")
-socket.emit('new-user-joined', name)
-
+const name = prompt("Enter your name to Join");
+socket.emit("new-user-joined", name);
